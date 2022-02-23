1f3b84869e47b4d97f25f2bdea91588f8aca1dab
Nick Bennett <nick@nickbenn.com>
2022-02-23 10:03:09 -0700

    Added EFF word list.

--- src/main/java/edu/cnm/deepdive/diceware/service/DicewareGenerator.java
+++ src/main/java/edu/cnm/deepdive/diceware/service/DicewareGenerator.java
@@ -3,12 +3,17 @@ package edu.cnm.deepdive.diceware.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DicewareGenerator implements PassphraseGenerator {
 
   private final Random rng;
   private final List<String> words;
 
+  @Autowired
   public DicewareGenerator(Random rng, WordProvider provider) {
     this.rng = rng;
     words = new ArrayList<>(provider.getWords());
@@ -16,11 +21,11 @@ public class DicewareGenerator implements PassphraseGenerator {
 
   @Override
   public String[] generate(int length) {
-    String[] selection = new String[length];
-    for (int i = 0; i < selection.length; i++) {
-      selection[i] = words.get(rng.nextInt(words.size()));
-    }
-    return selection;
+    return IntStream
+        .generate(() -> rng.nextInt(words.size()))
+        .limit(length)
+        .mapToObj(words::get)
+        .toArray(String[]::new);
   }
 
 }
